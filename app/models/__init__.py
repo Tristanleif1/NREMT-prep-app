a00c9171dbd18a9240ab302e3e316daa2e78a647
Tristanleif1 <tristanallaman@gmail.com>
2023-09-16 09:30:04 -0700

    Added models to __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,7 @@ from .db import environment, SCHEMA
 from .flashcard import Flashcard
 from .flashcardSet import FlashcardSet
 from .question import Question
+from .quiz_topic import quiz_topics
 from .quiz import Quiz
+from .topic import Topic
 
