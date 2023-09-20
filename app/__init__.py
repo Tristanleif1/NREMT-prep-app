efe6ea59a312e76ff4a9890290035bd018b14135
Tristanleif1 <tristanallaman@gmail.com>
2023-09-20 14:43:24 -0700

    Blueprints registered

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from .models import db, User, Flashcard, FlashcardSet, Question, Topic, quiz_top
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.flashcard_routes import flashcard_routes
+from .api.flashcard_sets_routes import flashcard_sets_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -33,6 +34,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(flashcard_routes, url_prefix='/api/flashcards')
+app.register_blueprint(flashcard_sets_routes, url_prefix='/api/flashcard-sets')
 db.init_app(app)
 Migrate(app, db)
 
