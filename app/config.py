71b140fde72816c655d471a8f619b2ee8247a8ec
Tristanleif1 <tristanallaman@gmail.com>
2023-09-20 16:00:03 -0700

    Moved env to root directory, can now use postman and get csrf token

--- app/config.py
+++ app/config.py
@@ -8,9 +8,10 @@ class Config:
     # (only 'postgresql') but heroku's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here (for production)
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    if DATABASE_URL:
-        SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://')
-    else:
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    # DATABASE_URL = os.environ.get('DATABASE_URL')
+    # if DATABASE_URL:
+    #     SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://')
+    # else:
+    #     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
