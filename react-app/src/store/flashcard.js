436fecdb0761a87c8d17f3e55d4b6c0daacb3a1b
Tristanleif1 <tristanallaman@gmail.com>
2023-09-21 11:25:03 -0700

    Flashcard store,reducer thunks etc set up

---
+++ react-app/src/store/flashcard.js
@@ -0,0 +1,135 @@
+import { csrfFetch } from "./csrf";
+const SET_FLASHCARDS = "flaschard/SET_FLASHCARDS"
+const SET_SINGLE_FLASHCARD ="flashcard/SET_SINGLE_FLASHCARD"
+const ADD_FLASHCARD = "flashcard/ADD_FLASHCARD"
+const EDIT_FLASHCARD = "flashcard/EDIT_FLASHCARD"
+const DELETE_FLASHCARD = "flashcard/DELETE_FLASHCARD"
+
+const setFlashcards = (flashcards) => ({
+    type: SET_FLASHCARDS,
+    flashcards
+})
+
+const addFlashcard = (flashcard) => ({
+    type: ADD_FLASHCARD,
+    flashcard
+})
+
+const editFlashcard = (flashcard) => ({
+    type: EDIT_FLASHCARD,
+    flashcard
+})
+
+const deleteFlashcard = (flashcardId) => ({
+    type: DELETE_FLASHCARD,
+    flashcardId
+})
+
+const setFlashcard = (flashcard) => ({
+    type: SET_SINGLE_FLASHCARD,
+    flashcard
+})
+
+
+export const loadFlashcards = () => async (dispatch) => {
+    const response = await fetch('/api/flashcards/')
+    if(response.ok){
+        const data = await response.json();
+        dispatch(setFlashcards(data.flashcards))
+    } else {
+        console.error("Failed to load flashcards")
+    }
+}
+
+export const createFlashcard = (flashcard) => async (dispatch) => {
+    const response = await csrfFetch('/api/flashcards', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(flashcard)
+    })
+    if (response.ok){
+        const createdFlashcard = await response.json();
+        dispatch(addFlashcard(createdFlashcard))
+    } else {
+        console.log(response, "Failed");
+        return response
+    }
+}
+
+
+export const updateFlashcard = (flashcard) => async (dispatch) => {
+    const response = await csrfFetch(`/api/flashcards/${flashcard.id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(flashcard)
+    });
+    const data = await response.json();
+    if(data.error){
+        throw new Error(data.error)
+    }
+    dispatch(editFlashcard(data))
+}
+
+
+export const removeFlashcard = (flashcardId) => async (dispatch) => {
+    const response = await csrfFetch(`/api/flashcards/${flashcardId}`, {
+        method: "DELETE"
+    })
+    if(response.ok){
+        dispatch(deleteFlashcard(flashcardId))
+    } else {
+        const error = await response.json();
+        console.error(error.message)
+    }
+};
+
+export const getSingleFlashcard = (flashcardId) => async (dispatch) => {
+    try {
+        const res = await fetch(`/api/flaschards/${flashcardId}`);
+        if(!response.ok){
+            throw new Error("Internal server error")
+        }
+
+        const flashcard = await res.json();
+        dispatch(setFlashcard(flashcard))
+    } catch (error){
+        console.error("Unable to fetch the flashcard", error)
+    }
+}
+
+export const flashcardsReducer = (state = {}, action) => {
+    switch(action.type){
+        case SET_FLASHCARDS:
+            const newState = {};
+            action.flashcards.forEach(flashcard => {
+                newState[flashcard.id] = flashcard
+            })
+            return newState;
+        case ADD_FLASHCARD:
+            return {
+                ...state,
+                [action.flashcard.id]: action.flashcard
+            }
+        case EDIT_FLASHCARD:
+            return {
+                ...state,
+                [action.flashcard.id]: action.flashcard
+            }
+        case SET_SINGLE_FLASHCARD:
+            return {
+                ...state,
+                [action.flashcard.id]: action.flashcard
+            }
+        case DELETE_FLASHCARD:
+            const copyState = { ...state };
+            delete copyState[action.flashcardId];
+            return copyState;
+        default:
+            return state;
+    }
+
+}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import { flashcardsReducer } from './flashcard';
 
 const rootReducer = combineReducers({
   session,
+  flashcard: flashcardsReducer
 });
 
 
