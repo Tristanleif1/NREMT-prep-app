c17cefcba840b215902731b7c668c154e01d993c
Tristanleif1 <tristanallaman@gmail.com>
2023-09-23 22:23:48 -0700

    Added logo and navbar

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
-import { logout } from "../../store/session";
+import { logout, login } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function ProfileButton({ user }) {
+  const history = useHistory();
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
+
+  const handleDemo = () => {
+    const email = "demo@aa.io";
+    const password = "password";
+    dispatch(login(email, password));
+    setShowMenu(false);
+  };
 
   const openMenu = () => {
     if (showMenu) return;
@@ -32,6 +41,8 @@ function ProfileButton({ user }) {
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
+    setShowMenu(false);
+    history.push('/')
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -42,17 +53,28 @@ function ProfileButton({ user }) {
       <button onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className={ulClassName} ref={ulRef}>
+      <ul className={user ? ulClassName : `${ulClassName} no-user`} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
-            <li>{user.email}</li>
-            <li>
+            <li className="user-information">Greetings,{user.username}!</li>
+            <li className="user-information">{user.email}</li>
+            <li className="user-information">
+              <Link to='/my-flashcards'>
+                View My Flashcards
+                </Link>
+            </li>
+            <li className="user-information">
+              <Link to='/my-flashcard-sets'>
+                View My Flashcard Sets
+              </Link>
+            </li>
+            <li className="list-button">
               <button onClick={handleLogout}>Log Out</button>
             </li>
           </>
         ) : (
           <>
+          <div className="signup-login">
             <OpenModalButton
               buttonText="Log In"
               onItemClick={closeMenu}
@@ -64,6 +86,8 @@ function ProfileButton({ user }) {
               onItemClick={closeMenu}
               modalComponent={<SignupFormModal />}
             />
+            <button onClick={handleDemo}>Demo User</button>
+            </div>
           </>
         )}
       </ul>
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,22 +3,33 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from "../../assets/149766631.png"
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
+		<>
+		  <div className="nav-bar">
+			{sessionUser ? (
+			  <NavLink className="create-flashcard" exact to="/new-flashcard">
+				Create a Flashcard
+			  </NavLink>
+			) : (
+			  <NavLink exact to="/login" className="create-project">
+				Create a Flashcard
+			  </NavLink>
 			)}
-		</ul>
-	);
-}
+			<div className="emt-logo">
+			  <NavLink className="emt-logo" exact to="/">
+				<img alt="rescure ready" src={logo}></img>
+			  </NavLink>
+			</div>
+			{isLoaded && <ProfileButton user={sessionUser} />}
+		  </div>
+		</>
+	  );
+	}
+	
 
 export default Navigation;
