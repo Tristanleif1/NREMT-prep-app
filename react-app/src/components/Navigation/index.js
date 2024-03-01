f9af41a79771d732fc080553ead7590a2f1c8124
Tristanleif1 <tristanallaman@gmail.com>
2024-03-01 15:54:14 -0800

    Working on navigation bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
