e1da93e86daf9ea6e16c7a79334a1862058dee65
Tristanleif1 <tristanallaman@gmail.com>
2024-08-08 13:51:55 -0700

    Added another quiz

--- app/seeds/quizzes.py
+++ app/seeds/quizzes.py
@@ -10,9 +10,19 @@ def seed_quizzes():
         userId=2,
         title="NREMT Practice Quiz #2"
     )
+    quiz3 = Quiz(
+        userId = 3,
+        title="Trauma Quiz"
+    )
+    quiz4 = Quiz(
+        userId= 4,
+        title="EMS Operations Quiz"
+    )
 
     db.session.add(quiz1)
     db.session.add(quiz2)
+    db.session.add(quiz3)
+    db.session.add(quiz4)
     db.session.commit()
 
 
